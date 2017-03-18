07fa9010d5ce0ae509a202886b873274fdca9cf0
KERNGRUPPE\nk.andkra90 <andreas.krahl@netzkern.de>
2017-03-18 15:44:17 +0100

    clean up js a little

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 var btn;
 var input;
-var curretTab = {};
-function init() {
-    btn = document.querySelector('button');
-    input = document.querySelector('input');
-    btn.addEventListener('click', function(e,a,b) {
-        var inputValue = input.value;      
-        
-        if (currentTab && currentTab.url) {
-            alert("Hello, " + inputValue + " your current URL is: " + currentTab.url);
-        }
-        else {
-            alert("CANNOT IDENTIFY URL; FORCING SHUTDOWN");
-        }
-    })
+var currentTab = {};
+
+var app = {
+    init: function () {
+        btn = document.querySelector('button');
+        input = document.querySelector('input');
+
+        chrome.tabs.getSelected(null, function (tab) {
+            currentTab = tab;
+        })
+
+        btn.addEventListener('click', function (e, a, b) {
+            var inputValue = input.value;
+
+            if (currentTab && currentTab.url) {
+                alert("Hello, " + inputValue + " your current URL is: " + currentTab.url);
+            } else {
+                alert("CANNOT IDENTIFY URL; FORCING SHUTDOWN");
+            }
+        });
+    }
 }
 
-(function() {   
-    chrome.tabs.getSelected(null, function(tab) { currentTab = tab; })
+document.addEventListener("DOMContentLoaded", function () {
 
-    
-})();
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    
-  init();
+    app.init();
 });

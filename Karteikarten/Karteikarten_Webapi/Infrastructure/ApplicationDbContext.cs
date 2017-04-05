b5d7c8de43ef26eb63d74a3ee7b010cd6c6dbf4e
KERNGRUPPE\nk.andkra90 <andreas.krahl@netzkern.de>
2017-04-05 17:29:05 +0200

    Refactor/ Add local ajax testing to validate authentication

---
+++ Karteikarten/Karteikarten_Webapi/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Karteikarten_Webapi.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Karteikarten.WebApi.Infrastructure
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext()
+            : base("web", throwIfV1Schema: false)
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+    }
+}

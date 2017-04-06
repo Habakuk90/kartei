3da05fb8bec6507ef1540ed17ff7784daefb4e47
nkRibbi <patrick.ribbach@netzkern.de>
2017-04-06 14:15:32 +0200

    edited the valid languages and made a question area

--- app/main.js
+++ app/main.js
@@ -1,8 +1,21 @@
-//variables 
+//jqstuff 
+$(document).ready(function () {
+    if ($('.translator').length) {
+        translateButtonClick();
+    }
+    refreshVars();
+    questionButtonClick();
+    closeButtonClick();
+});
+
+
+
+//translation Area
 var $input,
     $output,
     $searchInputSelect,
     $searchOutputSelect,
+    $quest,
     url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?',
     apiKey = 'trnsl.1.1.20170406T074435Z.462745397ec2fd4b.9f45661cedc89156721292f8ca682f261d05efa8',
     text = '&text=',
@@ -18,15 +31,8 @@ var refreshVars = function () {
     $output = $('#search-output');
     $searchInputSelect = $('#search-input-select');
     $searchOutputSelect = $('#search-output-select');
+    $quest = $('.question');
 }
-
-var buttonClick = function () {
-    $('#search-button').on('click', function () {
-        refreshVars();
-        yandex();
-    })
-};
-
 var yandex = function () {
     var xhr = new XMLHttpRequest(),
         from = $searchInputSelect.val(),
@@ -40,21 +46,53 @@ var yandex = function () {
     xhr.onreadystatechange = function () {
 
         if (this.readyState == 4 && this.status == 200) {
-            var res = this.responseText;
-            console.log(res);
-            var json = JSON.parse(res);
+            var response = this.responseText;
+            console.log(response);
+            var json = JSON.parse(response);
             if (json.code == 200) {
                 console.log(json);
-                $output.html(json.text[0]);
+                if (json.text[0] == $input.val()) {
+                    $output.html('Sorry but the Word is too similiar');
+                } else {
+                    $output.val(json.text[0]);
+                }
 
             } else {
                 console.log('Error Code: ' + json.code);
             }
+        } else {
+            $output.val('Sorry but something went wrong!');
         }
     }
 }
+var translateButtonClick = function () {
+    $('#search-button').on('click', function () {
+        refreshVars();
+        yandex();
+    })
+};
 
-$(document).ready(function () {
-    buttonClick();
 
-});
+// end of translation Area
+
+//Question Area
+var questionButtonClick = function () {
+    var $btnQuestion = $('.btn-question');
+    $btnQuestion.on('click', function () {
+
+        $quest.addClass('is-active');
+
+
+    });
+}
+var closeButtonClick = function () {
+    var $btnClose = $('.btn-close');
+    $btnClose.on('click', function () {
+        $quest.removeClass('is-active');
+
+    })
+
+}
+
+
+//end of Question Area
